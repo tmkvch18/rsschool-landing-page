1d2e70240dc750bf0698c8ddf402a05ced7010c3
tmkvch18 <tmkvch18@gmail.com>
2026-09-21 23:12:44 +0300

    feat: add theme switcher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@ import "../styles/style.scss";
 import initBurgerMenu from "./burgerMenu.js";
 import initSlider from "./slider.js";
 import initMenuTabs from "./menuTabs.js";
+import initThemeSwitcher from "./themeSwitcher.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initBurgerMenu();
   initSlider();
   initMenuTabs();
+  initThemeSwitcher();
 });
